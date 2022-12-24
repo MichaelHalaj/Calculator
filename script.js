d7e676e19f8b28be5a6219a64af49b2a36635a9a
Michael Halaj <michaelhalaj7@gmail.com>
2022-12-23 22:36:39 -0500

    Create operate function

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ function multiply(val1, val2){
 function divide(val1, val2){
     return val1 / val2;
 }
+
+function operate(operator, val1, val2){
+    switch(operator){
+        case '+':
+            return add(val1, val2);
+        case '-':
+            return subtract(val1, val2);
+        case '*':
+            return multiply(val1, val2);
+        case '/':
+            return divide(val1, val2);
+        default:
+            return 'Invalid input';
+    }
+}
