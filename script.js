93a7fb2fdbfc9a6237c2fc280c44555a904c084b
Michael Halaj <michaelhalaj7@gmail.com>
2022-12-24 13:34:13 -0500

    Display not working as intended

--- script.js
+++ script.js
@@ -31,13 +31,37 @@ function operate(operator, val1, val2){
             return 'Invalid input';
     }
 }
-function clearText(){
-    
+function clearValues(){
+    const displayNum = document.querySelector('display-value');
+    if(displayNum){
+        display.removeChild(displayNum);
+    }
+    return;
+}
+
+/**
+ * Gets current display element or creates a new display if one does not exist.
+ */
+function getCurrentDisplayElement(){
+
+    const displayNum = document.querySelector('display-value');
+    console.log(display);
+    if(displayNum){
+        console.log('Should be here');
+        return displayNum;
+    }else{
+        const newDisplayNum = document.createElement('div');
+        newDisplayNum.classList.add('display-value');
+        display.appendChild(newDisplayNum);
+        return newDisplayNum;
+    }
+
 }
 function displayNumber(num){
-    const displayNum = document.createElement('div');
-    displayNum.innerText = num;
-    display.appendChild(displayNum);
+    const displayNum = getCurrentDisplayElement();
+    let currVal = displayNum.innerText;
+    displayNum.innerText = `${currVal}${num}`;
+    console.log(currVal + '' +num);
 }
 
 
